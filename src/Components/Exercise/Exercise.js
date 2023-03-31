10a20c7c4b03043a55e91e905b7dfe81eeb13f17
jihad <mdjihadislam166@gmail.com>
2023-03-31 23:40:26 +0600

    added

--- src/Components/Exercise/Exercise.js
+++ src/Components/Exercise/Exercise.js
@@ -1,14 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import './Exercise.css'
 import Product from '../Product/Product';
+import Cart from '../Cart/Cart';
 
 const Exercise = () => {
     const [products,setproduct] = useState([]);
+    const [cart,setcart] = useState([]);
+
     useEffect(()=>{
         fetch('https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json')
         .then(response => response.json())
         .then(data => setproduct(data))
     },[])
+
+
+   // handle button click //
+    const AddToProduct = (product) => {
+        console.log(product);
+        const newCart = [...cart,product];
+        setcart(newCart);
+      };
+
+
     return (
         <div className='exercise-container'>
             <div className="product-container">
@@ -16,13 +29,14 @@ const Exercise = () => {
                     products.map(product =><Product 
                         key={product.id}
                         product={product}
+                        AddToProduct={AddToProduct}
 
                     ></Product>)
                 }
             <p>Lorem ipsum dolor sit. {products.length}</p>
             </div>
             <div className="cart-container">
-            <p>Lorem ipsum dolor sit</p>
+                <Cart cart={cart}></Cart>
             </div>
         </div>
     );
--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,21 +1,29 @@
 import React from "react";
 import "./Product.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee,faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
 const Product = (props) => {
-  console.log(props.product);
+  console.log(props);
   const { name, img, seller, price, ratings } = props.product;
+
 
   return (
     <div className="product">
       <img src={img} alt="" />
       <div>
         <p>{name}</p>
-        <p>${price}</p>
-        <p><small>{seller}</small></p>
-        <p><small>{ratings} stars</small></p>
+        <p>price: ${price}</p>
+        <p>
+          <small>Seller:{seller}</small>
+        </p>
+        <p>
+          <small>ratings:{ratings} stars</small>
+        </p>
       </div>
-      <button className="btn-cart">
-        <p>Add To Cart</p>
+      <button onClick={() => props.AddToProduct(props.product)}  className="btn-cart">
+        <p className="btn-text">Add To Cart</p>
+        <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
       </button>
     </div>
   );
